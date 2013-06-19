c831592af3e3c9c7de527d88f9177889ce49c445
Anthony Goddard <anthony@anthonygoddard.com>
2013-06-19 00:51:20 -0400

    added initial poller code, updated readme

---
+++ docker/poller.rb
@@ -0,0 +1,20 @@
+require 'mongo/driver'
+
+os = %w(jdoi34j djwoief fj93jg4 jfio23jf)
+language = %w(rb pl py sh)
+
+
+while true do
+  puts "polling mongo"
+  mongo_uri = 'mongodb://***REMOVED***'
+  calculations = Mongo::Connection.from_uri(mongo_uri).db('app16386574').collection('calculations')
+
+  calculations.all answer: nil do |calculation|
+    puts "found #{calculation['_id']}"
+    puts "processing #{calculation['calculation']} with #{os.sample} and #{language.sample}"
+    puts %x(./docker -d /opt/calculate.#{language} --calculation='#{calculation['calculation']}' --id=#{calculation['_id']})
+    #parse response from docker
+    #update MongoDB
+  end
+  sleep 10
+end
