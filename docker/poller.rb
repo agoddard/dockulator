fab6c5403556c79ae993c4db798b53e846d07cff
Anthony Goddard <anthony@anthonygoddard.com>
2013-06-19 21:48:03 -0400

    changed polling to look for processed

--- docker/poller.rb
+++ docker/poller.rb
@@ -9,12 +9,13 @@ while true do
   mongo_uri = 'mongodb://***REMOVED***'
   calculations = Mongo::Connection.from_uri(mongo_uri).db('app16386574').collection('calculations')
 
-  calculations.all answer: nil do |calculation|
+  calculations.all processed: false do |calculation|
+    #optionally rate limit based on # on requests
     puts "found #{calculation['_id']}"
     puts "processing #{calculation['calculation']} with #{os.sample} and #{language.sample}"
     puts %x(./docker -d /opt/calculate.#{language} --calculation='#{calculation['calculation']}' --id=#{calculation['_id']})
     #parse response from docker
     #update MongoDB
   end
-  sleep 10
+  sleep 2
 end
