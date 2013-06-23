64b15cfe16a3b20962557a7d077f077af80139be
Anthony Goddard <anthony@anthonygoddard.com>
2013-06-22 23:49:35 -0400

    inital pass at ruby calculator

---
+++ calculators/calc.rb
@@ -0,0 +1,11 @@
+left, op, right = ARGV[0].split
+
+allowed = ["+","-","*","/"]
+
+if allowed.include? op
+  answer = left.to_f.send(op, right.to_f)
+  puts answer
+else
+  exit 1
+  puts "Error"
+end
