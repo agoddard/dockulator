89c9fbb605b219fde9cfc0c86d3308ffd8fdc1fd
Chuck Ha <ha.chuck@gmail.com>
2013-07-29 23:00:09 -0400

    pointers!

--- docker/poller.go
+++ docker/poller.go
@@ -44,7 +44,7 @@ func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	var result []calc.Calculation
-	jobs := make(chan calc.Calculation)
+	jobs := make(chan *calc.Calculation)
 	go ThrottledJobs(jobs)
 
 	for {
@@ -56,7 +56,7 @@ func main() {
 			job := result[i]
 			job.Language = PickString(languages)
 			job.OS = PickString(oses)
-			jobs <- job
+			jobs <- &job
 		}
 		time.Sleep(pollDelay * time.Second)
 	}
@@ -66,7 +66,7 @@ func PickString(slice []string) string {
 	return slice[rand.Int()%len(slice)]
 }
 
-func ThrottledJobs(jobs chan calc.Calculation) {
+func ThrottledJobs(jobs chan *calc.Calculation) {
 	for job := range jobs {
 		<-throttle
 		log.Printf("Processing %s using %s on %s\n", job.Calculation, job.Language, job.OS)
@@ -75,7 +75,7 @@ func ThrottledJobs(jobs chan calc.Calculation) {
 	}
 }
 
-func StartJob(calculation calc.Calculation) {
+func StartJob(calculation *calc.Calculation) {
 	cmd := exec.Command(dockerPath, "run", calculation.OS, "/opt/dockulator/calculators/calc."+calculation.Language,  calculation.Calculation)
 	if debug {
 		log.Printf("args: %v", strings.Join(cmd.Args, " "))
