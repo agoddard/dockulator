4dde1b808880971b76581cce7624b1455b514390
Chuck Ha <ha.chuck@gmail.com>
2013-08-01 13:55:23 -0400

    Updates package names for the example file

--- example.go
+++ example.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/ChuckHa/calculations/calculations"
-	"github.com/ChuckHa/calculations/db"
-	"labix.org/v2/mgo/bson"
+	"dockulator/models"
+	"dockulator/db"
+//	"labix.org/v2/mgo/bson"
 )
 
 func main () {
@@ -16,29 +16,32 @@ func main () {
 	/*
 	// Save 10 new calculations
 	for i := 0 ; i < 10; i++ {
-		calc := calculations.NewCalculation(fmt.Sprintf("%d + 9", i))
-		calc.Save(c)
+		calc := models.NewCalculation(fmt.Sprintf("%d + 9", i))
+		calc.Save()
 	}
 	*/
 
 	/*
 	// Save 1 new calculation and print it
-	calc := calculations.NewCalculation("9 + 3")
-	calc.Save(c)
+	calc := models.NewCalculation("9 + 3")
+	calc.Save()
 	fmt.Println(calc)
 	*/
 
 	var err error
 	// Get all objects
-	var result []calculations.Calculation
-	err = c.Find(bson.M{"instance": ""}).All(&result)
+	var result models.Calculations
+	err = c.Find(nil).Sort("-time").Limit(4).All(&result)
 	if err != nil {
 		panic(err)
 	}
 
 	// Print each object
-	fmt.Println(result)
+	//fmt.Println(result)
 
+	for i := 0; i < len(result); i++ {
+		fmt.Println(string(result[i].Json()))
+	}
 	/*
 	// Print each objects _id
 	for i := 0; i < len(result); i++ {
@@ -48,22 +51,24 @@ func main () {
 		fmt.Println(result[i].Escaped())
 	}
 	*/
+	/*
 	for i := 0; i< len(result); i++ {
 		fmt.Println(result[i].Instance)
 		result[i].Instance = "HELLO WORLD"
 		c.Update(bson.M{"_id": result[i].Id},bson.M{"$set": bson.M{"instance": "value"}})
 	}
+	*/
 
 	/*
 	// Find an object by ID
-	result := calculations.Calculation{}
+	result := models.Calculation{}
 	c.FindId(bson.ObjectIdHex("51c1bb61022c206ec22aca60")).One(&result)
 	fmt.Println(result)
 	*/
 
 	/*
 	// Get one object and update it
-	result := calculations.Get(0, c)
+	result := models.Get(0)
 	result.OS = "CentOS"
 	result.Language = "Python"
 	fmt.Println(result)
