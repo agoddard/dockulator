1137bf67cc06b95dc1f9e3ee516fae40be90e6c2
Chuck Ha <ha.chuck@gmail.com>
2013-08-01 19:00:47 -0400

    Look for env variables before you just crap out

--- db/db.go
+++ db/db.go
@@ -1,8 +1,20 @@
 package db
 
 import (
+	"fmt"
 	"os"
 	"labix.org/v2/mgo"
+)
+
+func init () {
+	mongoUrl = os.Getenv("MONGO_URL")
+	if mongoUrl == "" {
+		panic(fmt.Errorf("Must supply a MONGO_URL env variable"))
+	}
+}
+
+var (
+	mongoUrl string
 )
 
 const (
