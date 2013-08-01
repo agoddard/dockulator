08cb9026519553682759a5b3938c936ad7280b3d
Chuck Ha <ha.chuck@gmail.com>
2013-07-31 21:26:33 -0400

    Moving stuff around

---
+++ server.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/ChuckHa/calculations/calculations"
+	"log"
+	"fmt"
+	"html/template"
+	"net/http"
+	"regexp"
+)
+
+const (
+	port       = "8080"
+	basePath   = "/calculations/"
+	lenPath	   = len(basePath)
+	collection = "calculations"
+	//logFile    = "/var/log/dockulator/error.log"
+)
+
+// Database collection
+var c = calculations.GetSession().DB("").C(collection)
+
+// A valid calculation
+var calcRe = regexp.MustCompile(`^\s*\d+ [\+\-\*\/] \d+\s*$`)
+
+// Templates
+var indexTmpl = template.Must(template.ParseFiles("templates/base.html", "templates/index.html"))
+var listTmpl = template.Must(template.ParseFiles("templates/base.html", "templates/calculations.html"))
+var detailTmpl = template.Must(template.ParseFiles("templates/base.html", "templates/calculation_detail.html"))
+
+// Handlers
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	indexTmpl.Execute(w, nil)
+}
+
+func calculationsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		calculation := r.FormValue("calculation")
+		found := calcRe.FindString(calculation)
+		if found != "" {
+			// Save the calculation in MongoDB
+			calc := calculations.NewCalculation(calculation)
+			calc.Insert(c)
+		} else {
+			http.Error(w, "Invalid calculation", 400)
+		}
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+	var results []calculations.Calculation
+	iter := c.Find(nil).Iter()
+	err := iter.All(&results)
+	if err != nil {
+		log.Println("Error getting calculations from mongodb:", err)
+	}
+	listTmpl.Execute(w, results)
+}
+func calculationsIdHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[lenPath:]
+	calc, err := calculations.Get(id, c)
+	if err != nil {
+		log.Println("Error getting a single calculation from mongodb:", err)
+	}
+	detailTmpl.Execute(w, calc)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/calculations", calculationsHandler)
+	http.HandleFunc("/calculations/", calculationsIdHandler)
+	listeningPort := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(listeningPort, nil)
+	fmt.Println("Listening on port", port)
+}
