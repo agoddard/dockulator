ff15d76f1656992b336ea068daa5c20c434a0844
Chuck Ha <ha.chuck@gmail.com>
2013-08-04 07:56:28 -0400

    Adds error state for calculations
    * Makes websockets better
    * Has poller try and hit the webserver after a calc is complete

--- models/calculations.go
+++ models/calculations.go
@@ -8,6 +8,7 @@ import (
 
 type Calculations []Calculation
 
+// Get recent calcuations where error is empty
 func GetRecent(n int) Calculations {
 	session := db.GetSession()
 	defer session.Close()
@@ -15,18 +16,20 @@ func GetRecent(n int) Calculations {
 	c := session.DB("").C(db.Collection)
 
 	result := Calculations{}
-	err := c.Find(nil).Sort("-time").Limit(n).All(&result)
+	err := c.Find(bson.M{"error": ""}).Sort("-time").Limit(n).All(&result)
 	if err != nil {
 		log.Printf("Got an error finding recent calculations: %v", err.Error())
 	}
 	return result
 }
 
+// Get all unsolved calculations.
+// An unsolved calculation has no instance nor an error
 func UnsolvedCalculations() (result Calculations) {
 	session := db.GetSession()
 	defer session.Close()
 
 	col := session.DB("").C(db.Collection)
-	col.Find(bson.M{"instance": ""}).All(&result)
+	col.Find(bson.M{"instance": "", "error": nil}).All(&result)
 	return result
 }
